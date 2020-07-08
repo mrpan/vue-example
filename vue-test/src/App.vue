876d4c1930fb2c62c395fdf35d6dcdc49eebb55f
mrpan <543421341@qq.com>
2020-07-08 22:43:07 +0800

    add CLI

---
+++ vue-test/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld /> -->
+    
+    <SlotTest url="/profile">
+    <template v-slot:header>
+      <h1>Here might be a page title</h1>
+    </template>
+    </SlotTest>
+    
+  </div> 
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import SlotTest from './components/SlotTest.vue'
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    SlotTest
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-test/src/components/SlotTest.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <header>
+    <slot name="header"></slot>
+  </header>
+  <main>
+    <slot name="test1"></slot>
+  </main>
+  <footer>
+    <slot name="footer"></slot>
+  </footer>
+  <a v-bind:href="url" class="nav-link">
+    <slot name="test2">this is slot</slot>
+  </a>
+</div>
+</template>
+
+<script>
+export default {
+  name: "SlotTest",
+  props: {
+    url: String,
+  },
+};
+</script>
